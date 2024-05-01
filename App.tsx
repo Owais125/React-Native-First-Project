41baab14ce85e2ad75fde9b4ca059bf412b97e64
Owais Raza <owaisy949@gmail.com>
2024-05-01 13:17:15 -0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+
+return  <View >
+<Text > 
+  Hello World
+</Text>
+<Text>
+  Owais Raza
+</Text>
+<Text>
+ Demo Screen
+</Text>
+
+
+  </View>
+
+}
+
+
+export default App;
